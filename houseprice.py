b106e70f9cee1568808ec2c9aec4fe3f9b779c59
padival <31299075+padival@users.noreply.github.com>
2018-05-06 15:34:36 -0700

    Update houseprice.py

--- houseprice.py
+++ houseprice.py
@@ -1,1 +1,54 @@
+import pandas as pd
+import numpy as np
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
 
+import matplotlib.pyplot as plt
+plt.style.use(style='ggplot')
+plt.rcParams['figure.figsize'] = (10, 6)
+plt.hist(train.SalePrice, color='blue')
+plt.show()
+
+target = np.log(train.SalePrice)
+numeric_features = train.select_dtypes(include=[np.number])
+corr = numeric_features.corr()
+quality_pivot = train.pivot_table(index='OverallQual', values='SalePrice', aggfunc=np.median)
+
+categoricals = train.select_dtypes(exclude=[np.number])
+train['enc_street'] = pd.get_dummies(train.Street, drop_first=True)
+test['enc_street'] = pd.get_dummies(train.Street, drop_first=True)
+
+def encode(x): return 1 if x == 'Partial' else 0
+train['enc_condition'] = train.SaleCondition.apply(encode)
+test['enc_condition'] = test.SaleCondition.apply(encode)
+
+data = train.select_dtypes(include=[np.number]).interpolate().dropna()
+y = np.log(train.SalePrice)
+X = data.drop(['SalePrice', 'Id'], axis=1)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=.33)
+from sklearn import linear_model
+lr = linear_model.LinearRegression()
+model = lr.fit(X_train, y_train)
+predictions = model.predict(X_test)
+from sklearn.metrics import mean_squared_error
+actual_values = y_test
+plt.scatter(predictions, actual_values, alpha=.75,
+            color='b') #alpha helps to show overlapping data
+plt.xlabel('Predicted Price')
+plt.ylabel('Actual Price')
+plt.title('Linear Regression Model')
+plt.show()
+
+submission = pd.DataFrame()
+submission['Id'] = test.Id
+feats = test.select_dtypes(include=[np.number]).drop(['Id'], axis=1).interpolate()
+predictions = model.predict(feats)
+final_predictions = np.exp(predictions)
+
+print ("Original predictions are: \n", predictions[:5], "\n")
+print ("Final predictions are: \n", final_predictions[:5])
+
+submission['SalePrice'] = final_predictions
+submission.head()
+submission.to_csv('submission1.csv', index=False)
